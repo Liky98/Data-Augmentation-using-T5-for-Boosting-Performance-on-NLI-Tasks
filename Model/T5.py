bcd383d0e13e640b5bf67ff6fd5e115d302c02a2
LeeKihoon <leekihoon1689@gmail.com>
2022-03-28 13:23:11 +0900

    T5 Model

--- Model/T5.py
+++ Model/T5.py
@@ -2,17 +2,6 @@
 T5모델 가져오는 코드
 """
 
-# from transformers import T5Tokenizer, T5ForConditionalGeneration
-#
-# tokenizer = T5Tokenizer.from_pretrained("t5-11b")
-# model = T5ForConditionalGeneration.from_pretrained("t5-11b")
-#
-# input_ids = tokenizer("The <extra_id_0> walks in <extra_id_1> park", return_tensors="pt").input_ids
-# labels = tokenizer("<extra_id_0> cute dog <extra_id_1> the <extra_id_2>", return_tensors="pt").input_ids
-# # the forward function automatically creates the correct decoder_input_ids
-# loss = model(input_ids=input_ids, labels=labels).loss
-
-#%%
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
@@ -26,6 +15,8 @@ tokenizer.pad_token = tokenizer.eos_token  # to avoid an error
 task_prefix = "translate English to Korean : "
 sentences = ["The house is wonderful.", "I like to work in Lab."]  # use different length sentences to test batching
 inputs = tokenizer([task_prefix + sentence for sentence in sentences], return_tensors="pt", padding=True)
+labels = ['positive', 'negative']
+target = tokenizer(labels, return_tensors='pt')
 
 output_sequences = model.generate(
     input_ids=inputs["input_ids"],
