c99b434ef54c68a5a3f9f137ff96d0e1163a96b4
LeeKihoon <leekihoon1689@gmail.com>
2022-05-03 12:46:00 +0900

    데이터셋 증대 환경 구성 및 알고리즘 분석 최종

--- 5월_새로운시작/소량데이터로 테스트함/main.py
+++ 5월_새로운시작/소량데이터로 테스트함/main.py
@@ -6,26 +6,32 @@ import Decoder
 import save_excel
 
 seed.set_seed(42)
-file_name = "t5Large, trainData_1000, entailment, nucleus_sampling, length_200"
+file_name = "t5base, trainData_1000, contradiction, nucleus_sampling, data_20000"
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
-entailment, neutral, contradiction = data_processing.data_load()
+entailment, neutral, contradiction = data_processing.data_load() #연관, 모호, 모순
 
 model = model_train.model_train(
     device=device,
-    dataset= entailment[:1000],
+    dataset= contradiction[:1000],
     epochs=5,
     path=file_name,
     set_max_length= 200,
-    model_name='t5-large'
+    model_name='t5-base'
 )
 
 outputs = Decoder.generation_sentence(model=model,
-                                                  dataset=entailment[1000:1200],
-                                                  device=device,
-                                                  decoder_argorithm="nucleus_sampling",  #beam_search, nucleus_sampling
-                                                  model_name = 't5-large',
-                                                  setting_length= 200
-                                                  )
+                                      dataset=contradiction[1000: 21000],
+                                      device=device,
+                                      decoder_argorithm="nucleus_sampling",  #beam_search, nucleus_sampling
+                                      model_name = 't5-base',
+                                      setting_length= 200
+                                      )
 
 save_excel.save_csv(outputs, file_name)
+
+"""
+0502 18:09 연관 데이터셋 2만개 생성 돌림
+0502 21:23 모호 데이터셋 2만개 생성 돌림
+0503 00:49 모순 데이터셋 2만개 생성 돌림
+"""
