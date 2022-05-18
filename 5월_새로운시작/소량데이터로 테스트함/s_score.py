35135874c5e43ec4ee9566c4c542a26de893783f
LeeKihoon <leekihoon1689@gmail.com>
2022-05-18 14:41:37 +0900

    새로운 문장 3만개 생성 + 센텐스트랜스포머 문장 유사성 검사

--- 5월_새로운시작/소량데이터로 테스트함/s_score.py
+++ 5월_새로운시작/소량데이터로 테스트함/s_score.py
@@ -6,19 +6,28 @@ import pandas as pd
 from datetime import datetime
 def cos_simiraty(dataset) :
     processing_dataset = []
-    for data in tqdm(dataset, desc="Dataset 점수 체크중 : "):
-        data1 = text_performance_indicators.sentence_transformer(sentences=data)
+
+    for i in tqdm(range(len(dataset)), desc="Dataset 점수 체크중 : "):
+
+        data1 = text_performance_indicators.sentence_transformer(sentences=[dataset["premise"][i],dataset["hypothesis"][i]])
         cosine_scores = util.pytorch_cos_sim(data1, data1)
-        if data["label"] == 0 :
+        if dataset["label"][i] == 0 :
             if cosine_scores[0][1].item() >70 :
-                processing_dataset.append(data)
-        elif data["lable"] == 1 :
+                processing_dataset.append([dataset["premise"][i],dataset["hypothesis"][i],dataset["label"][i]])
+        elif dataset["label"][i] == 1 :
             if cosine_scores[0][1].item() >40 and cosine_scores[0][1].item() <70  :
-                processing_dataset.append(data)
-        elif data["lable"] == 2:
+                processing_dataset.append([dataset["premise"][i],dataset["hypothesis"][i],dataset["label"][i]])
+        elif dataset["label"][i] == 2:
             if cosine_scores[0][1].item() < 40 :
-                processing_dataset.append(data)
+                processing_dataset.append([dataset["premise"][i],dataset["hypothesis"][i],dataset["label"][i]])
 
     df = pd.DataFrame(processing_dataset, columns=["premise", "hypothesis", "label"])
-    df.to_csv('processing_{}.csv'.format(datetime.datetime.today().strftime("%D/%H:%M")), index=False)
+
+    df.to_csv('processing.csv', index=False)
     print(f" 데이터 증대 개수 : {len(df)}")
+
+if __name__ == "__main__":
+
+    dataset = "DA_(0517)t5base, trainData_500, nucleus_sampling, data_10000.csv"
+    dataset = pd.read_csv(dataset)
+    cos_simiraty(dataset)
