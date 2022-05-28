ee23a66afd69682827e8f0a641b74d6fd47f46ef
LeeKihoon <leekihoon1689@gmail.com>
2022-05-28 13:58:09 +0900

    뉴클리어스 샘플링 텍스트유사도 분류 후 원본과 합쳐서 실험

--- 5월_새로운시작/classification/main.py
+++ 5월_새로운시작/classification/main.py
@@ -7,17 +7,23 @@ import test
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 seed.set_seed(42)
 model_name= 'roberta-large'
-model_save_path = "save_model/100실험/"
+model_save_path = "save_model/뉴클텍스트유사도분류후원본합침/"
 
 #저장될 최종 데이터셋 경로 설정
-dataset_path = "dataset/100실험"
+dataset_path = "dataset/뉴클텍스트유사도분류후원본합침"
 
 #증대 데이터만 합쳐논 DataDict path
-da_train_csv_path = "../소량데이터로 테스트함/데이터셋/DA_val_(0517)t5base, trainData_500, nucleus_sampling, data_10000.csv"
-da_all_csv_path = "../소량데이터로 테스트함/데이터셋/점수판별실험전/DA_(0517)t5base, trainData_100, nucleus_sampling, data_10000.csv"
-dataset = data_loader.data_load_noRawDatset(final_dataset_path = dataset_path,
-                                     da_train_csv_path=da_all_csv_path
-                                     )
+da_train_csv_path = "../소량데이터로 테스트함/nucleus0526processing.csv"
+
+#da_all_csv_path = "../소량데이터로 테스트함/100Few_Nucleus실험/processing.csv"
+# print(da_all_csv_path)
+# dataset = data_loader.data_load_noRawDatset(final_dataset_path = dataset_path,
+#                                      da_train_csv_path=da_all_csv_path
+#                                      )
+
+print(da_train_csv_path)
+dataset = data_loader.snli_data_load(final_dataset_path=dataset_path,
+                                     da_train_csv_path=da_train_csv_path)
 
 train_dataloader, validation_dataloader, test_dataloader = data_loader.dataloader(model_name= model_name,
                                                                                   dataset=dataset
@@ -29,7 +35,7 @@ model_path, accuracy_mean_list = model_func.model_train(model_name= model_name,
                                               dev_dataloader= validation_dataloader,
                                               device= device,
                                               save_path= model_save_path,
-                                              num_epochs = 3,
+                                              num_epochs = 10,
                                               num_label = 3
                                               )
 
