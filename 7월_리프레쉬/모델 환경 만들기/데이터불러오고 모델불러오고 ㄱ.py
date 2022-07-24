ab2e88de3e0e6ba155456e1f53b309e3c25999b2
LeeKihoon <leekihoon1689@gmail.com>
2022-07-24 17:25:38 +0900

    NLP 모델 제작

--- 7월_리프레쉬/모델 환경 만들기/데이터불러오고 모델불러오고 ㄱ.py
+++ 7월_리프레쉬/모델 환경 만들기/데이터불러오고 모델불러오고 ㄱ.py
@@ -1,5 +1,6 @@
 from Hugging모델모음 import Model_load
 from Hugging_GLUE import GLUE_dataset
+from torch.utils.data import DataLoader, Dataset
 
 def dataset_name_list() :
     dataset = GLUE_dataset
@@ -9,6 +10,16 @@ def load_dataset_from_huggingface(dataset_name) :
     dataset = GLUE_dataset
     return dataset.load_GLUE_from_Huggingface(dataset_name)
 
+def dataset_batch(df):
+    labels = {'correct' : 0,
+              'x' : 1}
+
+    labels = [labels[label] for label in df['category']]
+    texts = [tokenizer(text, padding='max_length', max_length=512, truncation=True,
+                            return_tensors="pt") for text in df['text']]
+
+    return texts, labels
+
 def load_model_and_tokenizer() :
     model_class = Model_load
     model, tokenizer = model_class.albert_MaskedLM(None)
@@ -16,6 +27,10 @@ def load_model_and_tokenizer() :
 
 def tokenize_function(example):
     return tokenizer(example["premise"], example["hypothesis"], truncation=True)
+
+
+
+
 
 if __name__ == "__main__":
     dataset = load_dataset_from_huggingface("mnli")
