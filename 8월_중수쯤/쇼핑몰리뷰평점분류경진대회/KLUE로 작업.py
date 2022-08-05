a185eee5376b2b41b50ac946c4b0502012239adc
LeeKihoon <leekihoon1689@gmail.com>
2022-08-05 13:11:18 +0900

    자연어 대회

---
+++ 8월_중수쯤/쇼핑몰리뷰평점분류경진대회/KLUE로 작업.py
@@ -0,0 +1,231 @@
+
+from transformers import AlbertForSequenceClassification, AlbertConfig, AlbertTokenizer
+from transformers import RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer, DataCollatorWithPadding
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, classification_report
+from datasets import load_dataset, DatasetDict
+from torch import nn
+from torch.optim import Adam
+from tqdm import tqdm
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+
+def train(train_dataloader,val_dataloader):
+    model = AutoModelForSequenceClassification.from_pretrained("klue/roberta-large", num_labels=6)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    len_train_data = 22500
+    len_val_data = 2500
+
+    model.to(device)
+    optimizer = Adam(model.parameters(), lr=1e-5)
+
+    top_val_loss = 100
+    top_val_accuracy = 0
+    train_loss_list =[]
+    val_loss_list = []
+    train_acc_list = []
+    val_acc_list = []
+
+    for epoch in range(20):
+        model.train()
+        total_acc_train = 0
+        total_loss_train = 0
+        now_data_len = 0
+
+        train_dataloader = tqdm(train_dataloader, desc='Loading train dataset')
+        for i, batch in enumerate(train_dataloader):
+
+            optimizer.zero_grad()
+            batch = {k: v.to(device) for k, v in batch.items()}
+
+
+            output = model(input_ids=batch["input_ids"],
+                           token_type_ids=batch['token_type_ids'],
+                           attention_mask=batch['attention_mask'],
+                           labels=batch['labels'])
+
+            #batch_loss = lossfunction(output.logits, batch['labels'])
+            output.loss.backward()
+            optimizer.step()
+
+            predict = torch.argmax(output.logits, dim=-1)
+
+            total_loss_train = total_loss_train + output.loss.item()
+            acc = (predict == batch['labels']).sum().item()
+            total_acc_train += acc
+            now_data_len += len(batch['labels'])
+
+            train_dataloader.set_description("Loss %.04f Acc %.04f | step %d Epoch %d" % (output.loss, total_acc_train / now_data_len, i,epoch))
+
+        total_acc_val = 0
+        total_loss_val = 0
+        with torch.no_grad():
+            model.eval()
+            for batch in tqdm(val_dataloader):
+                batch = {k: v.to(device) for k, v in batch.items()}
+
+                output = model(**batch)
+
+                predict = torch.argmax(output.logits, dim=-1)
+
+                acc = (predict == batch['labels']).sum().item()
+
+                #batch_loss = lossfunction(output.logits, batch['labels'])
+
+                total_loss_val += output.loss.item()
+
+                total_acc_val += acc
+            print()
+            print(f'Epochs: {epoch + 1} \n'
+                  f'| Train Loss: {total_loss_train / len_train_data: .3f} \n'
+                  f'| Train Accuracy: {total_acc_train / len_train_data: .3f} \n'
+                  f'| Val Loss: {total_loss_val /len_val_data: .3f} \n'
+                  f'| Val Accuracy: {total_acc_val / len_val_data: .3f}')
+            train_acc_list.append(total_acc_train / len_train_data)
+            train_loss_list.append(total_loss_train / len_train_data)
+            val_acc_list.append(total_acc_val / len_val_data)
+            val_loss_list.append(total_loss_val /len_val_data)
+
+        # if total_loss_val > top_val_loss and total_acc_val < top_val_accuracy :
+        #
+        #     break
+        # if total_loss_val < top_val_loss:
+        #     top_val_loss = total_loss_val
+        # if total_acc_val > top_val_accuracy:
+        #     top_val_accuracy = total_acc_val
+        model_path = "./model" + str(epoch) + ".pth"
+        model.save_pretrained(model_path)
+
+    return model, train_acc_list, train_loss_list, val_acc_list, val_loss_list
+
+def data() :
+    data = pd.read_csv("./dataset/train.csv")
+    data['target'].unique()  # 1,2,4,5 만 있음.
+
+    raw_train = load_dataset('csv', data_files='./dataset/train.csv')
+    raw_test = load_dataset('csv', data_files='./dataset/test.csv')
+    train, valid = raw_train['train'].train_test_split(test_size=0.1).values()
+    review_dataset = DatasetDict({'train': train, 'valid': valid, 'test': raw_test['train']})
+
+    def tokenize_function(example):
+        return tokenizer(example["reviews"], truncation=True)
+
+    tokenized_datasets = review_dataset.map(tokenize_function, batched=True)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    tokenized_datasets = tokenized_datasets.remove_columns(["id", "reviews"])
+    tokenized_datasets['train'] = tokenized_datasets['train'].rename_column("target", "labels")
+    tokenized_datasets['valid'] = tokenized_datasets['valid'].rename_column("target", "labels")
+    tokenized_datasets.set_format("torch")
+
+    train_dataloader = DataLoader(tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator)
+    val_dataloader = DataLoader(tokenized_datasets["valid"], shuffle=True, batch_size=8, collate_fn=data_collator)
+    test_dataloader = DataLoader(tokenized_datasets["test"], shuffle=False, batch_size=8, collate_fn=data_collator)
+
+    return train_dataloader, val_dataloader, test_dataloader
+
+def final_test(model, test_dataloader):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    prediction_list = []
+    label_list = []
+    model.eval()
+    with torch.no_grad():
+        for batch in tqdm(test_dataloader):
+            outputs = model(**batch.to(device))
+
+            logits = outputs.logits
+            predictions = torch.argmax(logits, dim=-1)
+            prediction_list.extend(predictions.cpu().tolist())
+    submission = pd.read_csv("dataset/sample_submission.csv")
+    submission["target"] = prediction_list
+    submission.to_csv("submission.csv",index=False)
+    return prediction_list
+
+
+def confusion(prediction_list) :
+    # 혼동행렬
+    my_data = []
+    y_pred_list = []
+    for data in prediction_list :
+        for data2 in data :
+            my_data.append(data2.item())
+    for data in label_list :
+        for data2 in data :
+            y_pred_list.append(data2.item())
+
+    confusion_matrix(my_data, y_pred_list)
+
+    confusion_mx = pd.DataFrame(confusion_matrix(y_pred_list, my_data))
+    ax =sns.heatmap(confusion_mx, annot=True, fmt='g')
+    plt.title('confusion', fontsize=20)
+    plt.show()
+
+    print(f"precision : {precision_score(my_data, y_pred_list, average='macro')}")
+    print(f"recall : {recall_score(my_data, y_pred_list, average='macro')}")
+    print(f"f1 score : {f1_score(my_data, y_pred_list, average='macro')}")
+    print(f"accuracy : {accuracy_score(my_data, y_pred_list)}")
+    f1_score_detail= classification_report(my_data, y_pred_list,  digits=3)
+    print(f1_score_detail)
+
+def result_graph(train_acc_list, train_loss_list, val_acc_list, val_loss_list):
+    epochs = [x for x in range(len(train_loss_list))]
+    print(epochs)
+    plt.plot(epochs, train_loss_list, 'r', label='Training loss')
+    epochs = [x for x in range(len(val_loss_list))]
+    plt.plot(epochs, val_loss_list, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    epochs = [x for x in range(len(train_acc_list))]
+    plt.plot(epochs, train_acc_list, 'r', label='Training Accuracy')
+    epochs = [x for x in range(len(val_acc_list))]
+    plt.plot(epochs, val_acc_list, 'b', label='Validation Accuracy')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("klue/roberta-large")
+    train_dataloader, val_dataloader, test_dataloader = data()
+
+    model, train_acc_list, train_loss_list, val_acc_list, val_loss_list = train(train_dataloader, val_dataloader)
+    result_graph(train_acc_list, train_loss_list, val_acc_list, val_loss_list)
+    predict_list, label_list = final_test(model, test_dataloader)
+
+
+    # submission = pd.read_csv("dataset/sample_submission.csv")
+    # submission["target"] = predict_list
+    # submission.to_csv("submission.csv",index=False)
+    #%%
+    model = AutoModelForSequenceClassification.from_pretrained("model19.pth")
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    prediction_list = []
+    label_list = []
+    model.eval()
+    with torch.no_grad():
+        for batch in tqdm(test_dataloader):
+            outputs = model(**batch.to(device))
+
+            logits = outputs.logits
+            predictions = torch.argmax(logits, dim=-1)
+            prediction_list.extend(predictions.cpu().tolist())
+    submission = pd.read_csv("dataset/sample_submission.csv")
+    submission["target"] = prediction_list
+    submission.to_csv("submission3.csv",index=False)
+
+
+
---
+++ 8월_중수쯤/쇼핑몰리뷰평점분류경진대회/데이터분석.py
@@ -0,0 +1,201 @@
+import pandas as pd
+from konlpy.tag import Okt
+from tokenizers import SentencePieceBPETokenizer
+import os
+from transformers import RobertaConfig, DataCollatorWithPadding, RobertaForSequenceClassification, RobertaTokenizer
+from datasets import load_dataset, DatasetDict
+
+class tokenizer_class() :
+    def csv_to_text(self) :
+        data = pd.read_csv("dataset/train.csv")
+        train_pair = [(row[1], row[2]) for _, row in data.iterrows() if type(row[1]) == str]  # nan 제거
+
+        # 문장 및 라벨 데이터 추출
+        train_data  = [pair[0] for pair in train_pair]
+        train_label = [pair[1] for pair in train_pair]
+        print('data loading done!')
+        print('문장: %s' %(train_data[:3]))
+        print('라벨: %s' %(train_label[:3]))
+
+        # subword 학습을 위해 문장만 따로 저장
+        with open('train_tokenizer.txt', 'w', encoding='utf-8') as f:
+            for line in train_data:
+                f.write(line+'\n')
+
+        # subword 학습을 위해 문장만 따로 저장
+        with open('train_tokenizer.txt', 'r', encoding='utf-8') as f:
+            test_tokenizer = f.read().split('\n')
+        print(test_tokenizer[:3])
+
+        num_word_list = [len(sentence.split()) for sentence in test_tokenizer]
+        print('\n코퍼스 평균/총 단어 갯수 : %.1f / %d' % (sum(num_word_list)/len(num_word_list), sum(num_word_list)))
+        return test_tokenizer
+
+    def Mecab(self, data):
+        tokenizer = Okt()
+        print('Okt check :', tokenizer.morphs('어릴때보고 지금다시봐도 재밌어요ㅋㅋ'))
+
+        for_generation = False  # or normal
+
+        if for_generation:
+            # 1: '어릴때' -> '어릴, ##때' for generation model
+            total_morph = []
+            for sentence in data:
+                # 문장단위 mecab 적용
+                morph_sentence = []
+                count = 0
+                for token_mecab in tokenizer.morphs(sentence):
+                    token_mecab_save = token_mecab
+                    if count > 0:
+                        token_mecab_save = "##" + token_mecab_save  # 앞에 ##를 부친다
+                        morph_sentence.append(token_mecab_save)
+                    else:
+                        morph_sentence.append(token_mecab_save)
+                        count += 1
+                # 문장단위 저장
+                total_morph.append(morph_sentence)
+
+        else:
+            # 2: '어릴때' -> '어릴, 때'   for normal case
+            total_morph = []
+            for sentence in data:
+                # 문장단위 mecab 적용
+                morph_sentence = tokenizer.morphs(sentence)
+                # 문장단위 저장
+                total_morph.append(morph_sentence)
+
+        print(total_morph[:3])
+        print(len(total_morph))
+
+        # mecab 적용한 데이터 저장
+        # ex) 1 line: '어릴 때 보 고 지금 다시 봐도 재밌 어요 ㅋㅋ'
+        with open('after_mecab.txt', 'w', encoding='utf-8') as f:
+            for line in total_morph:
+                f.write(' '.join(line) + '\n')
+        with open('after_mecab.txt', 'r', encoding='utf-8') as f:
+            test_tokenizer = f.read().split('\n')
+
+        return test_tokenizer
+
+    # def add_special_tokens(self) :
+    #     user_defined_symbols = ['[BOS]', '[EOS]', '[UNK0]',
+    #                             '[UNK1]', '[UNK2]', '[UNK3]',
+    #                             '[UNK4]', '[UNK5]', '[UNK6]',
+    #                             '[UNK7]', '[UNK8]', '[UNK9]']
+    #     unused_token_num = 200
+    #     unused_list = ['[unused{}]'.format(n) for n in range(unused_token_num)]
+    #     user_defined_symbols = user_defined_symbols + unused_list
+    #
+    #     print(user_defined_symbols)
+    #     return user_defined_symbols
+
+    def train_tokenizer(self, tokenizer) :
+        corpus_file = ['after_mecab.txt']  # data path
+        vocab_size = 32000
+        limit_alphabet = 6000 #merge 수행 전 initial tokens이 유지되는 숫자 제한
+        # output_path = 'hugging_%d' % (vocab_size)
+        min_frequency = 2 # n회 이상 등장한 pair만 merge 수행
+
+        # Then train it!
+        tokenizer.train(files=corpus_file,
+                        vocab_size=vocab_size,
+                        min_frequency=min_frequency,  # 단어의 최소 발생 빈도, 5
+                        limit_alphabet=limit_alphabet,  # ByteLevelBPETokenizer 학습시엔 주석처리 필요
+                        show_progress=True,
+                        special_tokens=["<s>", "</s>", "<unk>", "<pad>", "<mask>"])
+        print('train complete')
+
+        sentence = '나는 오늘 아침밥을 먹었다.'
+        output = tokenizer.encode(sentence)
+        print(sentence)
+        print(output)
+
+        # save tokenizer
+        hf_model_path = 'liky_tokenizer'
+        if not os.path.isdir(hf_model_path):
+            os.mkdir(hf_model_path)
+
+        tokenizer.save_model(hf_model_path)  # vocab.txt 파일 한개가 만들어진다
+
+        return tokenizer
+
+    def test(self):
+        from transformers import RobertaTokenizer
+
+        tokenizer_for_load = RobertaTokenizer.from_pretrained('liky_tokenizer',
+                                                               strip_accents=False,  # Must be False if cased model
+                                                               lowercase=False)  # 로드
+
+        print('vocab size : %d' % tokenizer_for_load.vocab_size)
+        # tokenized_input_for_pytorch = tokenizer_for_load("i am very hungry", return_tensors="pt")
+        tokenized_input_for_pytorch = tokenizer_for_load("나는 오늘 아침밥을 먹었다.", return_tensors="pt")
+        tokenized_input_for_tensorflow = tokenizer_for_load("나는 오늘 아침밥을 먹었다.", return_tensors="tf")
+
+        print("Tokens (str)      : {}".format([tokenizer_for_load.convert_ids_to_tokens(s) for s in
+                                               tokenized_input_for_pytorch['input_ids'].tolist()[0]]))
+        print("Tokens (int)      : {}".format(tokenized_input_for_pytorch['input_ids'].tolist()[0]))
+        print("Tokens (attn_mask): {}\n".format(tokenized_input_for_pytorch['attention_mask'].tolist()[0]))
+
+
+if __name__ =="__main__":
+    tokenizer_class = tokenizer_class()
+
+    text_data = tokenizer_class.csv_to_text()
+    text_data = tokenizer_class.Mecab(text_data)
+
+    tokenizer = SentencePieceBPETokenizer()
+    tokenizer = tokenizer_class.train_tokenizer(tokenizer)
+
+    config = RobertaConfig(
+        vocab_size=22745,
+        max_position_embeddings=514,
+        num_attention_heads=12,
+        num_hidden_layers=6,
+        type_vocab_size=1,
+    )
+
+    model = RobertaForSequenceClassification(config=config)
+    print('Num parameters: ', model.num_parameters())
+    tokenizer = RobertaTokenizer.from_pretrained("./liky_tokenizer")
+    print(tokenizer)
+
+    raw_train = load_dataset('csv', data_files='./dataset/train.csv')
+    raw_test = load_dataset('csv', data_files='./dataset/test.csv')
+    train, valid = raw_train['train'].train_test_split(test_size=0.1).values()
+    dataset = DatasetDict({'train': train, 'valid': valid, 'test': raw_test['train']})
+    print(dataset)
+
+    print(tokenizer.tokenize(train['reviews'][0]))
+
+
+    def tokenize_function(example):
+        return tokenizer(example["reviews"], truncation=True)
+
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    print(tokenized_datasets)
+    tokenized_datasets = tokenized_datasets.remove_columns(["id", "reviews"])
+    tokenized_datasets['train'] = tokenized_datasets['train'].rename_column("target", "labels")
+    tokenized_datasets['valid'] = tokenized_datasets['valid'].rename_column("target", "labels")
+    tokenized_datasets.set_format("torch")
+    tokenized_datasets["train"].column_names
+
+    from torch.utils.data import DataLoader
+
+    train_dataloader = DataLoader(tokenized_datasets["train"], shuffle=True, batch_size=4,
+                                  collate_fn=data_collator)
+    valid_dataloader = DataLoader(tokenized_datasets["valid"], shuffle=True, batch_size=4,
+                                  collate_fn=data_collator)
+    eval_dataloader = DataLoader(tokenized_datasets["test"], shuffle=False, batch_size=4,
+                                 collate_fn=data_collator)
+
+    for batch in train_dataloader:
+        break
+    {k: v.shape for k, v in batch.items()}
+#%%
+tokenizer = RobertaTokenizer("./liky_tokenizer/vocab.json", "./liky_tokenizer/merges.txt")
+
+print(tokenizer.encode(train['reviews'][0]))
+train['reviews'][0]
+#tokenizer
